aebe42da72d4c36a9ce4b7f925857da33cb3cd14
changjiaming [常佳明] <changjiaming@xiaomi.com>
2019-11-27 20:02:31 +0800

    修复 Geolocation 中的引用

--- Libraries/Geolocation/Geolocation.js
+++ Libraries/Geolocation/Geolocation.js
@@ -15,7 +15,7 @@ const NativeEventEmitter = require('../EventEmitter/NativeEventEmitter');
 const RCTLocationObserver = require('../BatchedBridge/NativeModules').LocationObserver;
 
 const invariant = require('fbjs/lib/invariant');
-const logError = require('logError');
+const logError = require('../Utilities/logError');
 /* $FlowFixMe(>=0.54.0 site=react_native_oss) This comment suppresses an error
  * found when Flow v0.54 was deployed. To see the error delete this comment and
  * run Flow. */
